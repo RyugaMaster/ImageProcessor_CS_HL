e207071f398b7cc92306a432bfbadd7813a4ba5e
updog123 <obol03al@gmail.com>
2019-10-30 20:26:04 +0300

    Fixed brightness and tone, added all basic functionality

--- src/View/View.java
+++ src/View/View.java
@@ -8,17 +8,30 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("ImageProcessor");
         ImageChanger imageChanger = new ImageChanger("file:C:\\Users\\obole\\Downloads\\scenery.jpeg");
-        imageChanger.setBrightness(0.5);
-        imageChanger.setContrast(2000D);
+        //imageChanger.setBrightness(0.5);
+        //imageChanger.setTone(120);
+        //imageChanger.setTone(1.5);
+        imageChanger.toGrayScale();
+        imageChanger.saveImage("C:\\Users\\obole\\Downloads\\sceneryGrayscale.png");
+        /*int[] hist = imageChanger.getHistogramRed(5);
+        for(int i : hist)
+            System.out.print(i + " ");
+        System.out.println("");
+        imageChanger.setTone(100);
+        hist = imageChanger.getHistogramRed(5);
+        for(int i : hist)
+            System.out.print(i + " ");
+        System.out.println("");*/
         imageChanger.setLayoutX(600);
-        Image image = imageChanger.getImage();
-        ImageView imageView = new ImageView(image);
+        ImageView imageView = new ImageView("file:C:\\Users\\obole\\Downloads\\scenery.jpeg");
         Group root = new Group(imageView, imageChanger);
         primaryStage.setScene(new Scene(root, 1000, 500));
         primaryStage.show();
