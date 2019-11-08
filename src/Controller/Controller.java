9a5978f4a11c74f27d3b979f9b672b84df7dc745
RyugaMaster <37570574+RyugaMaster@users.noreply.github.com>
2019-11-08 09:54:26 +0300

    Adding safety measures to file dialogs

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,27 +1,31 @@
 package Controller;
 
+import java.io.File;
+
 public class Controller {
     public static void start() {
 
     }
 
-    public static void open()
-    {
-        System.out.println(View.Dialogs.openImage().toURI().toString());
+    public static void open() {
+        File f = View.Dialogs.openImage();
+        if (f != null) {
+            System.out.println(f.toURI().toString());
+        }
     }
 
-    public static void save()
-    {
+    public static void save() {
 
     }
 
-    public static void saveAs()
-    {
-        System.out.println(View.Dialogs.saveImage().toURI().toString());
+    public static void saveAs() {
+        File f = View.Dialogs.saveImage();
+        if (f != null) {
+            System.out.println(f.toURI().toString());
+        }
     }
 
-    public static void quit()
-    {
+    public static void quit() {
 
     }
 }
