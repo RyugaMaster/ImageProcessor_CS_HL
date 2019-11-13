dcd8c139cb4000361b23f2525297797961a85314
RyugaMaster <37570574+RyugaMaster@users.noreply.github.com>
2019-11-13 09:36:04 +0300

    Added imageView

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,16 +1,21 @@
 package Controller;
 
+import Model.ImageChanger;
+
 import java.io.File;
 
 public class Controller {
-    public static void start() {
+    static private ImageChanger imageChanger;
 
+    public static void start(ImageChanger _imageChanger) {
+        imageChanger = _imageChanger;
     }
 
     public static void open() {
         File f = View.Dialogs.openImage();
         if (f != null) {
-            System.out.println(f.toURI().toString());
+            imageChanger.setImage(f);
+            View.View.read();
         }
     }
 
