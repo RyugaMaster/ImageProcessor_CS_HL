5d966cb965314d9592fad3cf9ac93e80ef8803bc
updog123 <obol03al@gmail.com>
2019-11-17 00:35:13 +0300

    added black-and-white conversion, fixed brightness and tone fo sure

--- src/Model/ImageChanger.java
+++ src/Model/ImageChanger.java
@@ -15,15 +15,18 @@ public class ImageChanger {
     private Image currentImage;
     private Filter filter;
 
+    //create ImageChanger of image from specified url
     public ImageChanger(String url) {
         this.setImage(url);
         this.filter = new Filter();
     }
 
+    //create empty ImageChanger
     public ImageChanger() {
         this.filter = new Filter();
     }
 
+    //create imageChanger of specified image
     public ImageChanger(Image image) {
         this.setImage(image);
         this.filter = new Filter();
@@ -43,21 +46,43 @@ public class ImageChanger {
         boolean isGrayscale = this.filter.isGrayscale();
         double tone = this.filter.getTone();
         double brightness = this.filter.getBrightness();
-        if(isGrayscale) this.applyGrayScale();
+        boolean isBV = this.filter.isBV();
         if(tone != 0.5) this.applyTone(tone);
         if(brightness != 1D) this.applyBrightness(brightness);
+        if(isGrayscale) this.applyGrayScale();
+        if(isBV) this.applyBV();
     }
 
     //change filter to grayscale
     public void toGrayScale() {
-        this.filter.setGrayscale(true);
-        this.applyFilter();
+        if(!this.filter.isGrayscale()) {
+            this.filter.setGrayscale(true);
+            this.applyFilter();
+        }
     }
 
     //change image back from grayscale
     public void undoGrayScale() {
-        this.filter.setGrayscale(false);
-        this.applyFilter();
+        if(this.filter.isGrayscale()) {
+            this.filter.setGrayscale(false);
+            this.applyFilter();
+        }
+    }
+
+    //change filter to black and white
+    public void toBV() {
+        if(!this.filter.isBV()) {
+            this.filter.setBV(true);
+            this.applyFilter();
+        }
+    }
+
+    //change image back from black and white
+    public void undoBV() {
+        if(this.filter.isBV()) {
+            this.filter.setBV(false);
+            this.applyFilter();
+        }
     }
 
     //change filter brightness
@@ -89,15 +114,15 @@ public class ImageChanger {
             for (int y = 0; y < height; y++) {
                 int pixel = pixelReader.getArgb(x, y);
 
+                int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
                 int green = ((pixel >> 8) & 0xff);
                 int blue = (pixel & 0xff);
 
                 int grayLevel = (int) (0.2162 * (double)red + 0.7152 * (double)green + 0.0722 * (double)blue) / 3;
-                grayLevel = 255 - grayLevel;
-                int grayPixel = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+                int grayPixel = (alpha << 24) | (grayLevel << 16) | (grayLevel << 8) | grayLevel;
 
-                pixelWriter.setArgb(x, y, -grayPixel);
+                pixelWriter.setArgb(x, y, grayPixel);
             }
         }
         this.currentImage = grayImage;
@@ -119,17 +144,17 @@ public class ImageChanger {
             for (int y = 0; y < height; y++) {
                 int pixel = pixelReader.getArgb(x, y);
 
+                int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
                 int green = ((pixel >> 8) & 0xff);
                 int blue = (pixel & 0xff);
 
-                int brightRed = 255 - (int) Math.min((double)red * brightness, 255D);
-                int brightGreen = 255 - (int) Math.min((double)green * brightness, 255D);
-                int brightBlue = 255 - (int) Math.min((double)blue * brightness, 255D);
-                int brightPixel = (brightRed << 16) + (brightGreen << 8) + brightBlue;
-                if(brightPixel == 0 && pixel != 0 && brightness >= 1)
-                    pixelWriter.setColor(x, y, Color.WHITE);
-                else pixelWriter.setArgb(x, y, -brightPixel);
+                int brightRed = (int) Math.min((double)red * brightness, 255D);
+                int brightGreen = (int) Math.min((double)green * brightness, 255D);
+                int brightBlue = (int) Math.min((double)blue * brightness, 255D);
+                int brightPixel = (alpha << 24) | (brightRed << 16) | (brightGreen << 8) | brightBlue;
+
+                pixelWriter.setArgb(x, y, brightPixel);
             }
         }
         this.currentImage = brightImage;
@@ -152,19 +177,71 @@ public class ImageChanger {
 
                 int pixel = pixelReader.getArgb(x, y);
 
+                int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
                 int green = ((pixel >> 8) & 0xff);
                 int blue = (pixel & 0xff);
 
-                int TonedRed = 255 - Math.max((int) Math.min((double) red + tone, 255D), 0);
-                int TonedGreen = 255 - green;
-                int TonedBlue = 255 - Math.min((int) Math.max((double) blue - tone, 0D), 255);
-                int toned = 255 - (TonedRed << 16) - (TonedGreen << 8) - TonedBlue;
+                int tonedRed = Math.max((int) Math.min((double) red + tone, 255D), 0);
+                int tonedBlue = Math.min((int) Math.max((double) blue - tone, 0D), 255);
+                int toned = (alpha << 24) | (tonedRed << 16) | (green << 8) | tonedBlue;
 
                 pixelWriter.setArgb(x, y, toned);
             }
         }
         this.currentImage = tonedImage;
+    }
+
+    //check if a pixel is in bounds of an image of specified height and width
+    private boolean pointInBounds(int pointX, int pointY, int maxX, int maxY) {
+        return pointX < maxX && pointY < maxY && pointX >= 0 && pointY >= 0;
+    }
+
+    //set image to black and white
+    private void applyBV() {
+        Image sourceImage = this.getImage();
+
+        PixelReader pixelReader = sourceImage.getPixelReader();
+
+        int width = (int)sourceImage.getWidth();
+        int height = (int)sourceImage.getHeight();
+        int[][] carry = new int[width][height];
+
+        WritableImage BVImage = new WritableImage(width, height);
+
+        PixelWriter pixelWriter = BVImage.getPixelWriter();
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                int pixel = pixelReader.getArgb(x, y);
+
+                int red = ((pixel >> 16) & 0xff);
+                int green = ((pixel >> 8) & 0xff);
+                int blue = (pixel & 0xff);
+
+                int pixelColor = (red + green + blue) / 3 + carry[x][y];
+                int error;
+
+                if(pixelColor < 128) {
+                    error = pixelColor;
+                    pixelWriter.setColor(x, y, Color.BLACK);
+                }
+                else {
+                    error = pixelColor - 255;
+                    pixelWriter.setColor(x, y, Color.WHITE);
+                }
+
+                if(pointInBounds(x + 1, y, width, height)) carry[x + 1][y] += error >> 2;
+                if(pointInBounds(x + 2, y, width, height)) carry[x + 2][y] += error >> 3;
+                if(pointInBounds(x + 1, y + 1, width, height)) carry[x + 1][y + 1] += error >> 3;
+                if(pointInBounds(x + 2, y + 1, width, height)) carry[x + 2][y + 1] += error >> 4;
+                if(pointInBounds(x, y + 1, width, height)) carry[x][y + 1] += error >> 2;
+                if(pointInBounds(x - 1, y + 1, width, height)) carry[x - 1][y + 1] += error >> 3;
+                if(pointInBounds(x - 2, y + 1, width, height)) carry[x - 2][y + 1] += error >> 4;
+            }
+        }
+        this.currentImage = BVImage;
     }
 
     //get distribution of red across specified number of divisions
--- src/View/View.java
+++ src/View/View.java
@@ -14,29 +14,6 @@ public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("ImageProcessor");
-        ImageChanger imageChanger = new ImageChanger("file:C:\\Users\\obole\\Downloads\\scenery.jpeg");
-        imageChanger.setBrightness(2);
-        //imageChanger.toGrayScale();
-        //imageChanger.undoGrayScale();
-        imageChanger.setTone(0.5);
-        //imageChanger.toGrayScale();
-        imageChanger.saveImage("C:\\Users\\obole\\Downloads\\scenery.jpeg");
-        /*int[] hist = imageChanger.getHistogramRed(5);
-        for(int i : hist)
-            System.out.print(i + " ");
-        System.out.println("");
-        imageChanger.setTone(100);
-        hist = imageChanger.getHistogramRed(5);
-        for(int i : hist)
-            System.out.print(i + " ");
-        System.out.println("");*/
-        ImageView imageViewCurrent = new ImageView(imageChanger.getImage());
-        imageViewCurrent.setLayoutX(600);
-        ImageView imageViewDefault = new ImageView("file:C:\\Users\\obole\\Downloads\\scenery.jpeg");
-        Group root = new Group(imageViewDefault, imageViewCurrent);
-        primaryStage.setScene(new Scene(root, 1200, 500));
-        primaryStage.show();
     }
 
     public static void show(String[] args) {launch(args);}
