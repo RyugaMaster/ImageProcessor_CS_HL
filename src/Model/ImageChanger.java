a0b386dedb1863230e16a3e2727920277c78ade5
updog123 <obol03al@gmail.com>
2019-11-17 00:35:13 +0300

    added black-and-white conversion, fixed brightness and tone fo sure

--- src/Model/ImageChanger.java
+++ src/Model/ImageChanger.java
@@ -1,10 +1,7 @@
 package Model;
 
 import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.image.Image;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelWriter;
-import javafx.scene.image.WritableImage;
+import javafx.scene.image.*;
 import javafx.scene.paint.Color;
 
 import javax.imageio.ImageIO;
@@ -18,9 +15,21 @@ public class ImageChanger {
     private Image currentImage;
     private Filter filter;
 
+    //create ImageChanger of image from specified url
+    public ImageChanger(String url) {
+        this.setImage(url);
+        this.filter = new Filter();
+    }
+
+    //create empty ImageChanger
     public ImageChanger() {
-        super();
-        filter = new Filter();
+        this.filter = new Filter();
+    }
+
+    //create imageChanger of specified image
+    public ImageChanger(Image image) {
+        this.setImage(image);
+        this.filter = new Filter();
     }
 
     //return image to initial state
@@ -34,21 +43,46 @@ public class ImageChanger {
     //apply changes to image
     private void applyFilter() {
         this.currentImage = defaultImage;
-        this.applyBrightness(this.filter.getBrightness());
-        this.applyTone(this.filter.getTone());
-        if(this.filter.isGrayscale()) this.applyGrayScale();
+        boolean isGrayscale = this.filter.isGrayscale();
+        double tone = this.filter.getTone();
+        double brightness = this.filter.getBrightness();
+        boolean isBV = this.filter.isBV();
+        if(tone != 0.5) this.applyTone(tone);
+        if(brightness != 1D) this.applyBrightness(brightness);
+        if(isGrayscale) this.applyGrayScale();
+        if(isBV) this.applyBV();
     }
 
     //change filter to grayscale
     public void toGrayScale() {
-        this.filter.setGrayscale(true);
-        this.applyFilter();
+        if(!this.filter.isGrayscale()) {
+            this.filter.setGrayscale(true);
+            this.applyFilter();
+        }
     }
 
     //change image back from grayscale
     public void undoGrayScale() {
-        this.filter.setGrayscale(false);
-        this.applyFilter();
+        if(this.filter.isGrayscale()) {
+            this.filter.setGrayscale(false);
+            this.applyFilter();
+        }
+    }
+
+    //change filter to black and white
+    public void toBV() {
+        if(!this.filter.isBV()) {
+            this.filter.setBV(true);
+            this.applyFilter();
+        }
+    }
+
+    //change image back from black and white
+    public void undoBV() {
+        if(this.filter.isBV()) {
+            this.filter.setBV(false);
+            this.applyFilter();
+        }
     }
 
     //change filter brightness
@@ -59,6 +93,7 @@ public class ImageChanger {
 
     //change filter tone
     public void setTone(double tone) {
+        tone = 510 * tone - 255;
         this.filter.setTone(tone);
         this.applyFilter();
     }
@@ -79,22 +114,22 @@ public class ImageChanger {
             for (int y = 0; y < height; y++) {
                 int pixel = pixelReader.getArgb(x, y);
 
+                int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
                 int green = ((pixel >> 8) & 0xff);
                 int blue = (pixel & 0xff);
 
                 int grayLevel = (int) (0.2162 * (double)red + 0.7152 * (double)green + 0.0722 * (double)blue) / 3;
-                grayLevel = 255 - grayLevel;
-                int grayPixel = (grayLevel << 16) + (grayLevel << 8) + grayLevel;
+                int grayPixel = (alpha << 24) | (grayLevel << 16) | (grayLevel << 8) | grayLevel;
 
-                pixelWriter.setArgb(x, y, -grayPixel);
+                pixelWriter.setArgb(x, y, grayPixel);
             }
         }
         this.currentImage = grayImage;
     }
 
     //change brightness of image according to filter
-    public void applyBrightness(double brightness) {
+    private void applyBrightness(double brightness) {
         Image sourceImage = this.currentImage;
 
         PixelReader pixelReader = sourceImage.getPixelReader();
@@ -109,17 +144,17 @@ public class ImageChanger {
             for (int y = 0; y < height; y++) {
                 int pixel = pixelReader.getArgb(x, y);
 
+                int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
                 int green = ((pixel >> 8) & 0xff);
                 int blue = (pixel & 0xff);
 
-                int brightRed = 255 - (int) Math.min((double)red * brightness, 255D);
-                int brightGreen = 255 - (int) Math.min((double)green * brightness, 255D);
-                int brightBlue = 255 - (int) Math.min((double)blue * brightness, 255D);
-                int brightPixel = (brightRed << 16) + (brightGreen << 8) + brightBlue;
-                if(brightPixel == 0 && pixel != 0 && brightness >= 1)
-                    pixelWriter.setColor(x, y, Color.WHITE);
-                else pixelWriter.setArgb(x, y, -brightPixel);
+                int brightRed = (int) Math.min((double)red * brightness, 255D);
+                int brightGreen = (int) Math.min((double)green * brightness, 255D);
+                int brightBlue = (int) Math.min((double)blue * brightness, 255D);
+                int brightPixel = (alpha << 24) | (brightRed << 16) | (brightGreen << 8) | brightBlue;
+
+                pixelWriter.setArgb(x, y, brightPixel);
             }
         }
         this.currentImage = brightImage;
@@ -128,8 +163,6 @@ public class ImageChanger {
     //change tone of image according to filter
     private void applyTone(double tone) {
         Image sourceImage = this.getImage();
-
-        tone = 510 * tone - 255;
 
         PixelReader pixelReader = sourceImage.getPixelReader();
 
@@ -144,19 +177,71 @@ public class ImageChanger {
 
                 int pixel = pixelReader.getArgb(x, y);
 
+                int alpha = ((pixel >> 24) & 0xff);
                 int red = ((pixel >> 16) & 0xff);
                 int green = ((pixel >> 8) & 0xff);
                 int blue = (pixel & 0xff);
 
-                int TonedRed = 255 - Math.max((int)Math.min((double)red + tone, 255D), 0);
-                int TonedGreen = 255 - green;
-                int TonedBlue = 255 - Math.min((int)Math.max((double)blue - tone, 0D), 255);
-                int toned = 255 - (TonedRed << 16) - (TonedGreen << 8) - TonedBlue;
+                int tonedRed = Math.max((int) Math.min((double) red + tone, 255D), 0);
+                int tonedBlue = Math.min((int) Math.max((double) blue - tone, 0D), 255);
+                int toned = (alpha << 24) | (tonedRed << 16) | (green << 8) | tonedBlue;
 
                 pixelWriter.setArgb(x, y, toned);
             }
         }
         this.currentImage = tonedImage;
+    }
+
+    //check if a pixel is in bounds of an image of specified height and width
+    private boolean pointInBounds(int pointX, int pointY, int maxX, int maxY) {
+        return pointX < maxX && pointY < maxY && pointX >= 0 && pointY >= 0;
+    }
+
+    //set image to black and white
+    private void applyBV() {
+        Image sourceImage = this.getImage();
+
+        PixelReader pixelReader = sourceImage.getPixelReader();
+
+        int width = (int)sourceImage.getWidth();
+        int height = (int)sourceImage.getHeight();
+        int[][] carry = new int[width][height];
+
+        WritableImage BVImage = new WritableImage(width, height);
+
+        PixelWriter pixelWriter = BVImage.getPixelWriter();
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                int pixel = pixelReader.getArgb(x, y);
+
+                int red = ((pixel >> 16) & 0xff);
+                int green = ((pixel >> 8) & 0xff);
+                int blue = (pixel & 0xff);
+
+                int pixelColor = (red + green + blue) / 3 + carry[x][y];
+                int error;
+
+                if(pixelColor < 128) {
+                    error = pixelColor;
+                    pixelWriter.setColor(x, y, Color.BLACK);
+                }
+                else {
+                    error = pixelColor - 255;
+                    pixelWriter.setColor(x, y, Color.WHITE);
+                }
+
+                if(pointInBounds(x + 1, y, width, height)) carry[x + 1][y] += error >> 2;
+                if(pointInBounds(x + 2, y, width, height)) carry[x + 2][y] += error >> 3;
+                if(pointInBounds(x + 1, y + 1, width, height)) carry[x + 1][y + 1] += error >> 3;
+                if(pointInBounds(x + 2, y + 1, width, height)) carry[x + 2][y + 1] += error >> 4;
+                if(pointInBounds(x, y + 1, width, height)) carry[x][y + 1] += error >> 2;
+                if(pointInBounds(x - 1, y + 1, width, height)) carry[x - 1][y + 1] += error >> 3;
+                if(pointInBounds(x - 2, y + 1, width, height)) carry[x - 2][y + 1] += error >> 4;
+            }
+        }
+        this.currentImage = BVImage;
     }
 
     //get distribution of red across specified number of divisions
@@ -202,19 +287,15 @@ public class ImageChanger {
         return histogram;
     }
 
-    public void setImage(Image image) {
-        this.defaultImage = image;
-        this.currentImage = image;
-    }
-
     //set image from url
     public void setImage(String url) {
         this.defaultImage = new Image(url);
-        this.currentImage = Jarvis.process(this.defaultImage);
+        this.currentImage = this.defaultImage;
     }
 
-    public void setImage(File f){
-        setImage(f.toURI().toString());
+    public void setImage(Image image) {
+        this.defaultImage = image;
+        this.currentImage = image;
     }
 
     //get current image
@@ -222,32 +303,14 @@ public class ImageChanger {
         return currentImage;
     }
 
-    public Image getDefaultImage() {
-        return defaultImage;
-    }
-
     //save image to file
-    public void saveImage(String path) throws RuntimeException, IOException {
+    public void saveImage(String path) throws RuntimeException{
         File outputFile = new File(path);
-        if(outputFile.createNewFile()) {
-            BufferedImage bImage = SwingFXUtils.fromFXImage(this.getImage(), null);
-            try {
-                ImageIO.write(bImage, "png", outputFile);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    public void saveImage(File f) throws RuntimeException, IOException {
-        File outputFile = f;
-        if (outputFile.createNewFile()) {
-            BufferedImage bImage = SwingFXUtils.fromFXImage(this.getImage(), null);
-            try {
-                ImageIO.write(bImage, "png", outputFile);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        BufferedImage bImage = SwingFXUtils.fromFXImage(this.getImage(), null);
+        try {
+            ImageIO.write(bImage, "png", outputFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/View/View.java
+++ src/View/View.java
@@ -2,57 +2,23 @@ package View;
 
 import Model.ImageChanger;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class View extends Application {
 
-    static Stage stage;
-    static private ImageView img;
-    static private ImageView src;
-    static private ImageView proc;
-    static private ImageChanger imageChanger;
-
-    public static void start(String[] args, ImageChanger _imageChanger) {
-        imageChanger = _imageChanger;
-        launch(args);
-    }
-
-    public static void read() {
-        src.setImage(imageChanger.getDefaultImage());
-        img.setImage(imageChanger.getImage());
-        proc.setImage(imageChanger.getImage());
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        VBox root = fxmlLoader.load(getClass().getResource("layout.fxml").openStream());
-        ViewController viewController = fxmlLoader.getController();
-        stage = primaryStage;
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1600, 900));
-        primaryStage.show();
-        img = viewController.img;
-        src = viewController.src;
-        proc = viewController.proc;
-        Slider hue = viewController.hue;
-        Slider brightness = viewController.brightness;
-        hue.setMax(1);
-        hue.setMin(0);
-        hue.setValue(0.5);
-        brightness.setMin(0.5);
-        brightness.setMax(2);
-        brightness.setValue(1);
-        // img.setFitWidth(1366);
-        //img.setFitHeight(768);
     }
 
-    /*public static void main(String[] args) {
+    public static void show(String[] args) {launch(args);}
+
+    public static void main(String[] args) {
         launch(args);
-    }*/
+    }
 }
