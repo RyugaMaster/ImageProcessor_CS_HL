e33f2f8fcb53a4605b03581335eabb57e0d5a206
updog123 <obol03al@gmail.com>
2019-11-17 11:20:23 +0300

    improved histograms and efficiency

--- src/View/View.java
+++ src/View/View.java
@@ -14,6 +14,17 @@ public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("ImageProcessor");
+        ImageChanger imageChanger = new ImageChanger("file:C:\\Users\\obole\\Downloads\\scenery.jpeg");
+        int[] hist = imageChanger.getHistogramRed(8);
+        for(int i : hist)
+            System.out.print(i + " ");
+        ImageView imageViewCurrent = new ImageView(imageChanger.getImage());
+        imageViewCurrent.setLayoutX(600);
+        ImageView imageViewDefault = new ImageView("file:C:\\Users\\obole\\Downloads\\scenery.jpeg");
+        Group root = new Group(imageViewDefault, imageViewCurrent);
+        primaryStage.setScene(new Scene(root, 1200, 500));
+        primaryStage.show();
     }
 
     public static void show(String[] args) {launch(args);}
